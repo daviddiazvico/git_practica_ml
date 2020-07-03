68a3d4619cf069924e92c845881c4d04266e74a1
david.diaz <david.diaz@iic.uam.es>
2020-07-03 13:14:20 +0200

    issue2

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from sklearn.linear_model import SGDRegressor
-
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 def load_model():
-    estimator = SGDRegressor()
-    return estimator
+estimator = Pipeline([("std", StandardScaler()),
+("lr", SGDRegressor())])
+return estimator
