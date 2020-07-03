989cd158e52cf2cd2757342e17218a4c22f6eab4
doaa.samy <doaa.samy@iic.uam.es>
2020-07-03 13:09:47 +0200

    Haciendo pruebas

--- experiment.py
+++ experiment.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+import numpy as np
+from sklearn.model_selection import cross_validate
+
 from dataset import load_dataset
 from model import load_model
 
-
 X, y = load_dataset()
 estimator = load_model()
+cv_scores = cross_validate(estimator, X, y)
 estimator.fit(X, y)
 score = estimator.score(X, y)
 print(f"Train score: {score}")
+print(f"CV score: {np.mean(cv_scores['test_score'])} ({cv_scores['test_score']})")
